af5690337fd0039b744db7070fd44d4446b2c6df
Tiago Costa <ntc132@newhuaweiapstaff-10-57-73-63.ncl.ac.uk>
2024-01-23 12:51:21 +0000

    tidied up #includes into SimulationsDependencies.h

---
+++ src/SimulationDependencies.h
@@ -0,0 +1,47 @@
+/*
+ * SimulationDependencies.h
+ *
+ *  Created on: 23 Jan 2024
+ *      Author: ntc132
+ */
+
+#ifndef SRC_SIMULATIONDEPENDENCIES_H_
+#define SRC_SIMULATIONDEPENDENCIES_H_
+
+#include "Boundary.h"
+#include "BoundaryWind.h"
+#include "BoundaryZeroGradient.h"
+#include "BoundaryWindLinearisedEuler.h"
+#include "BoundaryWindPassiveScalar.h"
+
+#include "Equations.h"
+#include "EquationsEuler.h"
+#include "EquationsEulerPassiveScalar.h"
+#include "EquationsLinearisedEuler.h"
+
+#include "Grid.h"
+#include "Grid1D.h"
+#include "GridRadial.h"
+
+#include "InitialData.h"
+#include "InitialDataHomogeneous.h"
+#include "InitialDataHomogeneousLinearisedEuler.h"
+#include "InitialDataHomogeneousPassiveScalar.h"
+#include "InitialDataIsothermal.h"
+#include "InitialDataSodShock.h"
+
+#include "Output.h"
+#include "OutputEuler.h"
+#include "OutputEulerPassiveScalar.h"
+#include "OutputLinearisedEuler.h"
+
+#include "RiemannSolver.h"
+#include "RiemannSolverHLLC.h"
+#include "RiemannSolverRusanov.h"
+
+#include "ShockFinder.h"
+#include "ShockFinderEulerPassiveScalar.h"
+
+#include "SimulationConfig.h"
+
+#endif /* SRC_SIMULATIONDEPENDENCIES_H_ */
