765706a3c2a11f54ebb48d061fb420a1d6724d17
Tiago Costa <ntc132@newhuaweiapstaff-10-57-94-17.ncl.ac.uk>
2024-01-26 14:15:50 +0000

    tidy up BoundaryWindPassiveScalar

--- src/BoundaryWindPassiveScalar.cpp
+++ src/BoundaryWindPassiveScalar.cpp
@@ -10,18 +10,12 @@
 #include "Grid.h"
 
 void BoundaryWindPassiveScalar::setBoundaries(){
-	double p, rho, rhoPassiveScalar, u;
 
-	for(int i = 0; i < grid.nx + 2*grid.nGhost; i++){
-		if(i < grid.minXIndex){
-			grid.quantities[i][EquationsEuler::DENS]   = rhoWind;
-			grid.quantities[i][EquationsEuler::XMOM]   = rhoWind * velWind;
-			grid.quantities[i][EquationsEuler::ENERGY] = (static_cast<EquationsEuler*>(&equations))->totalEnergy(pressureWind, rhoWind * velWind * velWind);
+	BoundaryWind::setBoundaries();
+
+	for(int i = 0; i < grid.nx + 2*grid.nGhost; i++)
+		if(i < grid.minXIndex)
 			grid.quantities[i][EquationsEulerPassiveScalar::PASS]   = rhoWind;
-		}
-		else if(i > grid.maxXIndex)
-			grid.quantities[i] = grid.quantities[grid.maxXIndex];
-	}
 }
 
 BoundaryWindPassiveScalar::~BoundaryWindPassiveScalar() {
